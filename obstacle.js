7ea9f41bea0e8e0f305a74e176f0c6ad076a059c
Tom <trobinson4433@gmail.com>
2021-11-30 18:19:18 +0000

    Private Variables added

--- obstacle.js
+++ obstacle.js
@@ -1,14 +1,18 @@
 class obstacle {
+  #x
+  #y
+  #width
+  #height
   constructor(x,y,width,height){
-    this.x = x;
-    this.y = y;
-    this.width = width;
-    this.height = height;
+    this.#x = x;
+    this.#y = y;
+    this.#width = width;
+    this.#height = height;
     
-    this.lower = this.y + (this.height/2);
-    this.higher = this.y - (this.height/2);
-    this.left = this.x - (this.width/2);
-    this.right = this.x + (this.width/2);
+    this.lower = this.#y + (this.#height/2);
+    this.higher = this.#y - (this.#height/2);
+    this.left = this.#x - (this.#width/2);
+    this.right = this.#x + (this.#width/2);
     console.log(this.lower);
    
   }
